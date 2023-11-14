f3f19a35cd5dc9da1901b26391d867266f1d52c4
Rodrigo Marrafa <rodrigoacmarrafa@gmail.com>
2023-11-14 16:29:18 +0000

    Add project files.

---
+++ Exercício 10.sln/Form1.cs
@@ -0,0 +1,31 @@
+namespace Exercício_10.sln
+{
+    using Microsoft.VisualBasic;
+    public partial class Form1 : Form
+    {
+        public string password;
+        public string passwordcerta;
+        public Form1()
+        {
+            InitializeComponent();
+            passwordcerta = "password";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            password = Interaction.InputBox("Insira a palavra-passe","Password","******");
+            if (password == passwordcerta)
+            {
+                MessageBox.Show("Acesso válido");
+            }
+            else
+                MessageBox.Show("Acesso inválido");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            passwordcerta = Interaction.InputBox("Insira a nova palavra-passe");
+            MessageBox.Show("palavra-passe alterada com sucesso");
+        }
+    }
+}
